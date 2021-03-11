a5bdeae7dd937b22b16b166d196e9087c87e3b03
HeavyCrown <55136577+HeavyCrown@users.noreply.github.com>
2021-03-11 20:04:13 +0800

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,23 @@
+class practice {
+    public static int subarraySum(int[] nums, int k) {
+        int sum=0,count=0;
+        int i=0,j=0;
+        while(i<nums.length){
+            for(j=i;j<nums.length;j++){
+                sum+=nums[j];
+                if(sum == k){
+                    count++;
+                }
+            }
+            sum=0;
+            i++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = new int[]{1,-1,0};
+        int k=0;
+        System.out.println(subarraySum(nums,k));
+    }
+}
